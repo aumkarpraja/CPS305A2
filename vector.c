8f902ad02418c1c9e12075f77c706bd01b946a32
aumkarpraja <aumkarpraja@gmail.com>
2015-10-14 17:32:10 -0400

    File input works

--- vector.c
+++ vector.c
@@ -5,16 +5,31 @@
 
 int main(int argc, char **argv)
 {
- // read in file
- printf("filename: %s\n", argv[1]);
-
+    // read in file
+    printf("filename: %s\n", argv[1]);
+    
     FILE *file;
     file = fopen(argv[1], "r");
     char c;
- while ((c=fgetc(file))!=EOF)
- {
-     printf("%c", c);
- }
+
+    // read in file while it hasn't reached the end
+    while ((c=fgetc(file))!=EOF)
+    {
+        // new lines should seperate the size and items in the array
+        if (c == '\n')
+        {
+            printf("THERE WAS A NEW LINE\n");
+        }
+        
+        // ignore spaces
+        else if (c==' ')
+        {
+            printf("space");
+        }
+        else
+        printf("%c", c);
+        
+    }
 
 
   return 0;
