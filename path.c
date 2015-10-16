f1d060641c98864937411dd2e945d6e66385218f
aumkarpraja <aumkarpraja@gmail.com>
2015-10-16 12:50:55 -0400

    Aumkar's fixes to Lawrence's changes

--- path.c
+++ path.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "path.h"
 
 //pathRead function
 void pathRead(Path *P) {
-        printf("%d", V->size);
+        printf("%d", P->size);
         //printf("%lud ", sizeof(V) / sizeof(V[0]));
         for (int i = 0; i < sizeof(P) / sizeof(P[0]); i++) {
-                printf("%d ", P->item[i]);
+                printf("%d ", P->items[i]);
         }
 }
 //pathInit function
-void pathInit(Path *P) {
+void pathInit(Path *P, int size) {
         P->size = size;
-        P->item = malloc(sizeof(int) * P->size);
+        P->items = malloc(sizeof(int) * P->size);
 }
 
 //pathAdd function
 void pathAdd(Path *P, int var, int index) {
-	P->item[index] = var;
+	P->items[index] = var;
 }
 
 //pathPrint function
-void pathPrint(Vector *P) {
+void pathPrint(Path *P) {
         for (int i = 0; i < P->size; i++) {
-                printf("Vector Val: %d\n", P->item[i]);
+                printf("Vector Val: %d\n", P->items[i]);
         }
 
 }
